ec8894cd3afbd0128dce72404546768bde4ed48a
Roopesh <dcroopesh96@gmail.com>
2020-04-02 11:59:55 +0530

    [Roopesh]Refactor.added index and room view

--- project/ChatApp/views.py
+++ project/ChatApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'ChatApp/index.html')
+
+def room(request, room_name):
+    return render(request, 'ChatApp/room.html', {'room_name': room_name})
+
