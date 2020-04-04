e8ba48ee941ed246afc10793426a8d6ab3525c3d
Roopesh <dcroopesh96@gmail.com>
2020-04-04 15:48:41 +0530

    [Roopesh]Refactor.added 3 models

--- project/ChatApp/models.py
+++ project/ChatApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class chatapp(models.Model):
+    
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=30)
+    roomname = models.CharField(max_length=10)
+
+class p2p(models.Model):
+    
+    username = models.ForeignKey(User, on_delete=models.CASCADE,related_name="user")
+    message = models.CharField(max_length=30)
+    roomname = models.CharField(max_length=10)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE,related_name="sent")
+
+class room(models.Model):
+    user = models.CharField(max_length=10) 
