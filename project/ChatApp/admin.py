acc2dbe481a3dc1353e33625979ebd48619c181f
Roopesh <dcroopesh96@gmail.com>
2020-04-04 15:47:05 +0530

    [Roopesh]Refactor.added registered 3 models

--- project/ChatApp/admin.py
+++ project/ChatApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import chatapp,p2p,room
 # Register your models here.
+admin.site.register(chatapp)
+admin.site.register(p2p)
+admin.site.register(room)
