849da1d62d49a7f5f8f917b214e1ec9e81623245
Roopesh <dcroopesh96@gmail.com>
2020-04-04 15:49:13 +0530

    [Roopesh]Refactor.added routing path to p2p

--- project/ChatApp/urls.py
+++ project/ChatApp/urls.py
@@ -4,5 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('',index, name='index'),
-    path('<str:room_name>/',room, name='room'),
+    path('p2p/',p2pIndex,name='p2p_index'),
+    path('<str:room_name>/',roomm, name='room'),
+    path('p2p/<str:receiver>/',p2pRoom, name='p2proom'),
+    
 ]
+
+
