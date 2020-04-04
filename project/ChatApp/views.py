a7f46aa9d3f4a8ca35dac4550c56819b93e7188e
Roopesh <dcroopesh96@gmail.com>
2020-04-04 15:50:00 +0530

    [Roopesh]Refactor.added p2p index and room view

--- project/ChatApp/views.py
+++ project/ChatApp/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.views import View
+from .models import room
+@login_required(login_url='/login/')
 def index(request):
-    return render(request, 'ChatApp/index.html')
+    return render(request, 'ChatApp/index1.html',{'title' : 'Chat Room'})
 
-def room(request, room_name):
+def roomm(request, room_name):
     return render(request, 'ChatApp/room.html', {'room_name': room_name})
+
+@login_required(login_url='/login/')
+def p2pIndex(request):
+    loggedin_user = request.user
+    users = User.objects.filter(~Q(username=loggedin_user))
+    user_list = [] 
+    for each in users:
+        user_list.append(each.username)
+    
+    return render(request, 'ChatApp/p2pindex.html',{'title' : 'Users','users' : user_list })
+
+def p2pRoom(request,receiver):
+    user = room.objects.create(user=receiver)
+    user.save()
+    return render(request, 'ChatApp/p2pRoom.html', {'room_name': 'room'})
+
+# class p2pIndex(View):
+   
+#     @login_required(login_url='/login/')
+#     def get(self, request):
+#         loggedin_user = request.user
+#         users = User.objects.filter(~Q(username=loggedin_user))
+#         user_list = [] 
+#         for each in users:
+#             user_list.append(each.username)
+    
+#         return render(request, 'ChatApp/p2pindex.html',{'title' : 'Users','users' : user_list })
+
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             # <process form cleaned data>
+#             return HttpResponseRedirect('/success/')
+
+#         return render(request, self.template_name, {'form': form})
 
