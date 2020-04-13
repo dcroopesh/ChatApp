a84a276eebfd89aad5becc7f1c99e81220725573
Roopesh <dcroopesh96@gmail.com>
2020-04-13 12:44:39 +0530

    [Roopesh]Refactor.added new route

--- project/ChatApp/urls.py
+++ project/ChatApp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index, name='index'),
-    path('p2p/',p2pIndex,name='p2p_index'),
-    path('<str:room_name>/',roomm, name='room'),
-    path('p2p/<str:receiver>/',p2pRoom, name='p2proom'),
+    # path('',index, name='index'),
+    # path('p2p/',p2pIndex,name='p2p_index'),
+    # path('<str:room_name>/',roomm, name='room'),
+    #path('p2p/<str:receiver>/',roomm, name='p2proom'),
+    path('',p2pRoom.as_view(), name='room'),
     
 ]
 
