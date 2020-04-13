1657629f4fdabd0346db7bff204f63d711680a22
Roopesh <dcroopesh96@gmail.com>
2020-04-13 12:42:24 +0530

    [Roopesh]Refactor.added new route

--- project/ChatApp/routing.py
+++ project/ChatApp/routing.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from .consumers import ChatConsumer,P2P
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
+    # re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
+    # re_path(r'ws/p2pchat/(?P<room_name>\w+)/$', P2P),
     re_path(r'ws/p2pchat/(?P<room_name>\w+)/$', P2P),
 ]
